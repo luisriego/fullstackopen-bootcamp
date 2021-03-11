835011705884493947dd7a1f212d4f43864b2f66
luisriego <luisriego@hotmail.com>
2021-03-10 21:51:02 -0300

    code working properly

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,42 +1,31 @@
 import React from 'react'
 
-const Header = ({title}) => {
-  return <h1>{title}</h1>
-}
-
-const Content = ({part, exercices}) => {
-  return (
-    <p>
-      {part} {exercices}
-    </p>
-  )
-}
-
-const Total = ({text, s1, s2, s3}) => {
-  return (
-    <p>
-      {text} {s1 + s2 + s3}
-    </p>
-  )
-}
+import Header from './componets/Header'
+import {Content} from './componets/Parts'
+import Total from './componets/Total'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header title={course} />
-      <Content part={part1} exercices={exercises1} />
-      <Content part={part2} exercices={exercises2} />
-      <Content part={part3} exercices={exercises3} />
-
-      <Total text='Number of exercises' s1={exercises1} s2={exercises2} s3={exercises3} />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
---
+++ part1/courseinfo/src/componets/Total/index.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react';
+
+export default function Total({parts}) {
+    const [total, setTotal] = useState(0)
+    useEffect(() => {
+      let totalizador = 0
+      parts.map((item) =>
+        totalizador = item.exercises + totalizador 
+      )
+      setTotal(totalizador)
+    }, [parts])
+    
+
+    return (
+        <p>
+            Number of exercices {total}
+        </p>
+      )
+}
