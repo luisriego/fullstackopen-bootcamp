0951e5b00c3c6df6f8e46fa6d0f3d85147287fc7
luisriego <luisriego@hotmail.com>
2021-03-14 12:42:28 -0300

    Exercise 2.3 resolved

--- part1/courseinfo/src/componets/Total/index.js
+++ part1/courseinfo/src/componets/Total/index.js
@@ -1,19 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
 export default function Total({parts}) {
-    const [total, setTotal] = useState(0)
-    useEffect(() => {
-      let totalizador = 0
-      parts.map((item) =>
-        totalizador = item.exercises + totalizador 
-      )
-      setTotal(totalizador)
-    }, [parts])
-    
+    const reduceAssoc = parts
+      .map((course) => course.exercises)
+      .reduce((prev, next) => prev + next, 0)
 
     return (
         <p>
-            <strong>Total of exercices {total}</strong>
+            <strong>Total of {reduceAssoc} exercices</strong>
         </p>
       )
 }
